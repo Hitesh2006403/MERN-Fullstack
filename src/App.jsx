74e4215ae637ce64b1608efeb8b1d2d22968170d
Hitesh2006403 <hiteshav2006@gmail.com>
2026-09-18 14:52:56 +0530

    New file added

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import EventSection from "./Components/Eventsection";
+import Footer from "./Components/footer";
 function App(){
   return(
     <main className="app">
       <Navbar />
       <Hero />
       <EventSection />
+      <Footer />
       <section className="welcome-card">
       </section>
     </main>
---
+++ src/Components/footer.jsx
@@ -0,0 +1,11 @@
+function footer(){
+return (
+    <footer id="about" className="footer">
+        <h2> Campus Connect </h2>
+        <p> Discover participating universities and events across the country. </p>
+        <p> Connect with fellow students and explore new opportunities.</p>
+    </footer>
+
+);
+}
+export default footer;
