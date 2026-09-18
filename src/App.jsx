05fb13ac8ce41e2cf4ea9ad809fd23f5f793c7e4
Hitesh2006403 <hiteshav2006@gmail.com>
2026-09-18 11:06:30 +0530

    new Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 
+// import "./App.css";
+// function App(){
+//   const tarinerName="Nirmal";
+//   const trainingDay=3;
+//   return(
+//     <main className="app">
+//       <Navbar />
+//       <section className="welcome-card">
+//       <p className="day-label">React Day {trainingDay}</p>
+//       <h1 className="title">College Course Explorer </h1>
+//       <p>Trainer:{tarinerName}</p>
+//       <p>React Learning Project</p>
+//       <p>Used React,JSX,componets,CSS</p>
+//       </section>
+//     </main>       
+//   );
+// }
+// export default App;
+
 import "./App.css";
+import Navbar from "./Components/Navbar";
 function App(){
-  const tarinerName="Nirmal";
-  const trainingDay=3;
   return(
-    <main className="app">
+    <div>
       <Navbar />
-      <section className="welcome-card">
-      <p className="day-label">React Day {trainingDay}</p>
-      <h1 className="title">College Course Explorer </h1>
-      <p>Trainer:{tarinerName}</p>
-      <p>React Learning Project</p>
-      <p>Used React,JSX,componets,CSS</p>
-      </section>
-    </main>       
+      <main id = "home">
+        <h1>College Course Explorer</h1>
+        <p>React Learning Project</p>
+      </main>
+    </div>
   );
 }
 export default App;
