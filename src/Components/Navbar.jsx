2dbdedf0a2aef7d23c73a1d5dd47e30805990401
Hitesh2006403 <hiteshav2006@gmail.com>
2026-09-18 10:09:01 +0530

    Updsted file

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+// function Navbar() {
+//   return (
+//     <nav className="Navbar">
+//         <a className="brand-name" href="#home">
+//             brand-name
+//         </a>
+//           <div className="nav-links">
+//             <a href="#home">Home</a>
+//             <a href="#about">About</a>
+//             <a href="#contact">Contact</a>
+//           </div>
+//         </nav>
+//   );
+// }
+// export default Navbar;
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+        <h2> Campus connect</h2>
+        <div className="nav-links">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>  
+            <a href="#contact">Contact</a>
+        </div>
+    </nav>
+  );
+}
+export default Navbar;
