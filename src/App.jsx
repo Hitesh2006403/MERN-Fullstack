891845087a86017963ad4408ec5260f836a3ac8f
Hitesh2006403 <hiteshav2006@gmail.com>
2026-09-19 11:49:48 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,58 @@
 // }
 // export default App;
 
+// import "./App.css";
+// import Navbar from "./Components/Navbar";
+// import Hero from "./Components/Hero";
+// import EventSection from "./Components/Eventsection";
+// import Footer from "./Components/footer";
+
+// function App() {
+//   return (
+//     <main className="app">
+//       <Navbar />
+//       <Hero />
+//       <EventSection />
+//       <Footer />
+//     </main>
+//   );
+// }
+
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import EventForm from "./Components/EventForm";
 import EventSection from "./Components/Eventsection";
 import Footer from "./Components/footer";
+import { initialEvents } from "./Data/Event";
 
 function App() {
+  const [events, setEvents] = useState(initialEvents);
+
+  function handleAddEvent(newEvent) {
+    setEvents([...events, newEvent]);
+  }
+
   return (
-    <main className="app">
+    <div>
       <Navbar />
-      <Hero />
-      <EventSection />
+
+      <main id="home">
+        <Hero
+          title="Discover What Is Happening on Campus"
+          description="Find workshops, sports activities, club meetings, and opportunities to connect with other students."
+        />
+
+        <EventForm onAddEvent={handleAddEvent} />
+
+        <EventSection events={events} />
+      </main>
+
       <Footer />
-    </main>
+    </div>
   );
 }
 
 export default App;
+
